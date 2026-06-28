2ab93dbaff0173f1bc89ce4453282b13e0181b76
JohnJohnx4 <johnjohnx4@gmail.com>
2026-06-28 13:32:21 -0400

    Use supplied Star Wars timer logo

--- src/pages/timerControllerUtils.ts
+++ src/pages/timerControllerUtils.ts
@@ -3,7 +3,7 @@ import pokemonLogo from "../assets/timer-logos/pokemon-tcg-logo.svg";
 import yugiohLogo from "../assets/timer-logos/yugioh-trading-card-game-logo.png";
 import lorcanaLogo from "../assets/timer-logos/disney-lorcana-logo.png";
 import onePieceLogo from "../assets/timer-logos/one-piece-card-game-logo-white.png";
-import starWarsUnlimitedLogo from "../assets/timer-logos/star-wars-unlimited.svg";
+import starWarsUnlimitedLogo from "../assets/timer-logos/star-wars-unlimited-logo-white.png";
 import unionArenaLogo from "../assets/timer-logos/union-arena-logo.webp";
 import dragonBallFusionLogo from "../assets/timer-logos/dragon-ball-fusion-official-white.png";
 import digimonLogo from "../assets/timer-logos/digimon-card-game-logo.webp";
